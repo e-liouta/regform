772c79525afa6466704dafd0a6fc3c41d3859be6
e-liouta <effieliouta@gmail.com>
2023-04-14 14:28:24 +0300

    javascript changes on functions

--- form.js
+++ form.js
@@ -1,16 +1,4 @@
-const nameInput = document.getElementById('name');
-const surnameInput = document.getElementById('surname');
-const genderInput = document.getElementById('gender');
-const passwordInput = document.getElementById('password');
-const passwordConfirmationInput = document.getElementById('passwordConfirmation');
-const form = document.getElementById('myForm');
-      
-// Function to check if a string contains at least one uppercase letter, one lowercase letter, one number, and one special character
-function isValidPassword(password) {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.{8,10})/;
-    return regex.test(password);
-}
-      
+ 
 function myFunction() {
   var x = document.getElementById("password");
   if (x.type === "password") {
@@ -18,4 +6,11 @@ function myFunction() {
   } else {
     x.type = "password";
   }
+}
+
+function check() {
+  var input = document.getElementById('passwordConfirmation');
+  if (input.value != document.getElementById('password').value) {
+      alert("Passwords do not match");
+  } 
 }
