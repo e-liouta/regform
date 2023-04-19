a18944880761d717b7eb5dbc0a0d4b578910ced3
e-liouta <effieliouta@gmail.com>
2023-04-19 17:35:28 +0300

    Final script

--- form.js
+++ form.js
@@ -14,3 +14,27 @@ function check() {
       alert("Passwords do not match");
   } 
 }
+
+function checknameifnum() {
+  let nameInput = document.getElementById("name");
+  let nameError = document.getElementById("nameError");
+  let name = nameInput.value.trim();
+  let pattern = /^[a-zA-Z]+$/;
+  if (pattern.test(name)) {
+    nameError.innerHTML = "";
+  } else {
+    nameError.innerHTML = "Name should not contain numbers";
+  }
+}
+
+function checksurnameifnum() {
+  let surnameInput = document.getElementById("surname");
+  let surnameError = document.getElementById("surnameError");
+  let surname = surnameInput.value.trim();
+  let pattern = /^[a-zA-Z]+$/;
+  if (pattern.test(surname)) {
+    surnameError.innerHTML = "";
+  } else {
+    surnameError.innerHTML = "Surname should not contain numbers";
+  }
+}
